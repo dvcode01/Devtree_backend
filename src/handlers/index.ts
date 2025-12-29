4586da380aab07f4d51ebd4670b07e47f85e38b8
David <daviolivero2103@gmail.com>
2025-12-29 15:15:16 -0400

    feat(handlers): validate duplicate users

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,11 +2,19 @@ import { Request, Response } from "express";
 import User from "../models/User";
 
 const createAccount = async(req: Request, res: Response) => {
-    const user = new User(req.body);
+    const { email } = req.body;
+    const emailExist = await User.findOne({ email });
 
+    if(emailExist){
+        const error = new Error('Usuario ya registrado');
+        return res.status(409).json({msg: error.message, error: true});
+    }
+    
+    const user = new User(req.body);
+    
     try {
         await user.save();
-        res.json({msg: 'Usuario registrado correctamente'});
+        res.status(201).json({msg: 'Usuario registrado correctamente'});
     } catch (error) {
         console.error(error);
     }
