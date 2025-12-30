c900006d28b5e2ca6f90cd2153b36b3c68a7f3ad
David <daviolivero2103@gmail.com>
2025-12-29 23:06:43 -0400

    feat: create function for hash password

---
+++ src/utils/auth.ts
@@ -0,0 +1,8 @@
+import bcrypt from 'bcrypt';
+
+const hashPassword = async (password: string) => {
+    const saltPass = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, saltPass);
+
+    return hash;
+}
